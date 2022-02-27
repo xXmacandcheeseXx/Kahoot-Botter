42710a4ed0256b690a171c21e17b7d20917a6f02
xXmacandcheeseXx <94151338+xXmacandcheeseXx@users.noreply.github.com>
2022-02-27 17:56:01 -0500

    Source Files

--- src/v3.0.0 Source/Browser.cs
+++ src/v3.0.0 Source/Browser.cs
@@ -1,4 +1,4 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.IO;
--- src/v3.0.0 Source/Program.cs
+++ src/v3.0.0 Source/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Kahoot_Botter
 {
